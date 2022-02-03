804c401893cc5d91c394b1788518b86d96a4a3f8
ShaneKhant <58925990+0xsheinn@users.noreply.github.com>
2022-02-03 07:22:41 +0630

    Update SKrecon.sh

--- SKrecon.sh
+++ SKrecon.sh
@@ -1,103 +1,32 @@
 #!/bin/bash
 
-url=$1
-if [ ! -d "$url" ];then
-    mkdir $url
+domain=$1
+
+if [ -z "$domain" ]; then
+    echo "Usage: $0 example.com"
+    exit 1
 fi
-if [ ! -d "$url/recon" ];then
-    mkdir $url/recon
-fi
-#    if [ ! -d '$url/recon/eyewitness' ];then
-#        mkdir $url/recon/eyewitness
-#    fi
-if [ ! -d "$url/recon/scans" ];then
-    mkdir $url/recon/scans
-fi
-if [ ! -d "$url/recon/httprobe" ];then
-    mkdir $url/recon/httprobe
-fi
-if [ ! -d "$url/recon/potential_takeovers" ];then
-    mkdir $url/recon/potential_takeovers
-fi
-if [ ! -d "$url/recon/wayback" ];then
-    mkdir $url/recon/wayback
-fi
-if [ ! -d "$url/recon/wayback/params" ];then
-    mkdir $url/recon/wayback/params
-fi
-if [ ! -d "$url/recon/wayback/extensions" ];then
-    mkdir $url/recon/wayback/extensions
-fi
-if [ ! -f "$url/recon/httprobe/alive.txt" ];then
-    touch $url/recon/httprobe/alive.txt
-fi
-if [ ! -f "$url/recon/final.txt" ];then
-    touch $url/recon/final.txt
-fi
- 
-echo "[+] Scanning subdomains with assetfinder..."
-assetfinder $url >> $url/recon/assets.txt
-cat $url/recon/assets.txt | grep $1 >> $url/recon/final.txt
-rm $url/recon/assets.txt
- 
-#echo "[+] Double checking for subdomains with amass..."
-#amass enum -d $url >> $url/recon/f.txt
-#sort -u $url/recon/f.txt >> $url/recon/final.txt
-#rm $url/recon/f.txt
- 
-echo "[+] Searching for alive domains..."
-cat $url/recon/final.txt | sort -u | httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ':443' >> $url/recon/httprobe/a.txt
-sort -u $url/recon/httprobe/a.txt > $url/recon/httprobe/alive.txt
-rm $url/recon/httprobe/a.txt
- 
-echo "[+] Checking for possible subdomain takeover..."
- 
-if [ ! -f "$url/recon/potential_takeovers/potential_takeovers.txt" ];then
-    touch $url/recon/potential_takeovers/potential_takeovers.txt
-fi
- 
-subjack -w $url/recon/final.txt -t 100 -timeout 30 -ssl -c ~/go/src/github.com/haccer/subjack/fingerprints.json -v 3 -o $url/recon/potential_takeovers/potential_takeovers.txt
- 
-echo "[+] Scanning for open ports..."
-nmap -iL $url/recon/httprobe/alive.txt -T4 -oA $url/recon/scans/scanned.txt
- 
-echo "[+] Scraping wayback data..."
-cat $url/recon/final.txt | waybackurls >> $url/recon/wayback/wayback_output.txt
-sort -u $url/recon/wayback/wayback_output.txt
- 
-echo "[+] Pulling and compiling all possible params found in wayback data..."
-cat $url/recon/wayback/wayback_output.txt | grep '?*=' | cut -d '=' -f 1 | sort -u >> $url/recon/wayback/params/wayback_params.txt
-for line in $(cat $url/recon/wayback/params/wayback_params.txt);do echo $line'=';done
- 
-echo "[+] Pulling and compiling js/php/aspx/jsp/json files from wayback output..."
-for line in $(cat $url/recon/wayback/wayback_output.txt);do
-    ext="${line##*.}"
-    if [[ "$ext" == "js" ]]; then
-        echo $line >> $url/recon/wayback/extensions/js1.txt
-        sort -u $url/recon/wayback/extensions/js1.txt >> $url/recon/wayback/extensions/js.txt
-    fi
-    if [[ "$ext" == "html" ]];then
-        echo $line >> $url/recon/wayback/extensions/jsp1.txt
-        sort -u $url/recon/wayback/extensions/jsp1.txt >> $url/recon/wayback/extensions/jsp.txt
-    fi
-    if [[ "$ext" == "json" ]];then
-        echo $line >> $url/recon/wayback/extensions/json1.txt
-        sort -u $url/recon/wayback/extensions/json1.txt >> $url/recon/wayback/extensions/json.txt
-    fi
-    if [[ "$ext" == "php" ]];then
-        echo $line >> $url/recon/wayback/extensions/php1.txt
-        sort -u $url/recon/wayback/extensions/php1.txt >> $url/recon/wayback/extensions/php.txt
-    fi
-    if [[ "$ext" == "aspx" ]];then
-        echo $line >> $url/recon/wayback/extensions/aspx1.txt
-        sort -u $url/recon/wayback/extensions/aspx1.txt >> $url/recon/wayback/extensions/aspx.txt
-    fi
-done
- 
-rm $url/recon/wayback/extensions/js1.txt
-rm $url/recon/wayback/extensions/jsp1.txt
-rm $url/recon/wayback/extensions/json1.txt
-rm $url/recon/wayback/extensions/php1.txt
-rm $url/recon/wayback/extensions/aspx1.txt
-#echo "[+] Running eyewitness against all compiled domains..."
-#python3 EyeWitness/EyeWitness.py --web -f $url/recon/httprobe/alive.txt -d $url/recon/eyewitness --resolve
+
+echo "[+] Scanning subdomain with assetfinder"
+assetfinder --subs-only $domain | tee -a subdomain.txt
+echo "[+] Scanning subdomain from rapiddns.io"
+curl -s "https://rapiddns.io/subdomain/$domain?full=1#result" | grep "<td><a" | cut -d '"' -f 2 | grep http | cut -d '/' -f3 | sed 's/#results//g' | sort -u | tee -a subdomain.txt
+echo "[+] Scanning subdomain from dns.bufferover.run"
+curl -s https://dns.bufferover.run/dns?q=.$domain |jq -r .FDNS_A[]|cut -d',' -f2|sort -u | tee -a subdomain.txt
+echo "[+] Scanning subdomain from dns.bufferover.run"
+curl -s "https://riddler.io/search/exportcsv?q=pld:$domain" | grep -Po "(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | sort -u | tee -a subdomain.txt
+echo "[+] Scanning subdomain from jldc.me"
+curl -s "https://jldc.me/anubis/subdomains/$domain" | grep -Po "((http|https):\/\/)?(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | sort -u | tee -a subdomain.txt
+echo "[+] Scanning subdomain from securitytrails.com"
+curl -s "https://securitytrails.com/list/apex_domain/$domain" | grep -Po "((http|https):\/\/)?(([\w.-]*)\.([\w]*)\.([A-z]))\w+" | grep ".$domain" | sort -u | tee -a subdomain.txt
+echo "[+] Scanning subdomain from crt.sh"
+curl -s "https://crt.sh/?q=%25.$domain&output=json" | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u | tee -a subdomain.txt
+echo "[+] Finding live domain and removing duplicate"
+cat subdomain.txt | sort -u > unique-subdomains.txt
+cat unique-subdomains.txt | httprobe | tee live-domain.txt
+echo "[+] Screenshoting live domains..."
+cat unique-subdomains.txt | uniq | httpx -silent -mc 200,302,404,403,401,400 -threads 70 | cut -d "/" -f 3 | uniq | aquatone -scan-timeout 3000 -threads 5 -silent -screenshot-timeout 50000 -http-timeout 20000 -out subdomains-screenshots
+rm subdomain.txt
+count=$(cat live-domain.txt | wc -l)
+echo "[+] Got $count live subdomain."
+echo "by @l33t_En0ugh"
